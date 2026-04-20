898b6b4a54e5de231d923452846ace5dda417331
Biboulder <bibo.pruzzi@gmail.com>
2026-04-20 19:37:23 +0200

    basic formula implementation

--- formula.py
+++ formula.py
@@ -1,3 +1,34 @@
 # Propositional formula representation
 # Represent formulas as a tree of objects
 # need classes for: Atom, Not, And, Or, Implies, Biconditional
+
+class Formula:
+    pass
+
+class Atom(Formula):
+    def __init__(self, name):
+        self.name = name
+
+class Not(Formula):
+    def __init__(self, operand):
+        self.operand = operand
+
+class And(Formula):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Or(Formula):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Implies(Formula):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Biconditional(Formula):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
